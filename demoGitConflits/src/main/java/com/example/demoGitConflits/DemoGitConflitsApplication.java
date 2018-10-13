69c899bb001636a64ac87f240722ffea46db33a6
pkamesh <kameswarao.pesingi@gmail.com>
2018-10-13 16:15:31 +0530

    added

--- demoGitConflits/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
+++ demoGitConflits/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
@@ -8,5 +8,8 @@ public class DemoGitConflitsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGitConflitsApplication.class, args);
+		
+		//sdkfsljdkf
+		
 	}
 }
