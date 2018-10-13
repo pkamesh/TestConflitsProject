0f1a9b21bebc77615e74d1dbe153b81ffce67cb1
pkamesh <kameswarao.pesingi@gmail.com>
2018-10-13 16:23:19 +0530

    KSJDFH

--- demoGitConflits/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
+++ demoGitConflits/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
@@ -8,5 +8,9 @@ public class DemoGitConflitsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGitConflitsApplication.class, args);
+		
+		//SJKFJLSDJFKLSDJFLSJD
+		
+		
 	}
 }
