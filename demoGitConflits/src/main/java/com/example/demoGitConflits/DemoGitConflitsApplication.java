01a7ea9b68838dc36e781fb11c243b1ec9c2acb8
pkamesh <37564320+pkamesh@users.noreply.github.com>
2018-10-13 16:50:16 +0530

    Update DemoGitConflitsApplication.java

--- demoGitConflits/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
+++ demoGitConflits/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
@@ -9,7 +9,7 @@ public class DemoGitConflitsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGitConflitsApplication.class, args);
 		
-		//sdkggkgkgkgkfsljdkf
+		//sdkggkgkgkgk2222222fsljdkf
 		
 	}
 }
